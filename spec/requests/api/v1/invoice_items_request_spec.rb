a9337cc4955c898279293c9159b9ef05792ba004
snayrouz <samuel.nayrouz@gmail.com>
2018-06-02 19:19:53 -0600

    Add show test for invoice items

--- spec/requests/api/v1/invoice_items_request_spec.rb
+++ spec/requests/api/v1/invoice_items_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "Invoice Item API" do
-  it "can send a list of invoice items" do
+  it "can return a list of invoice items" do
     create_list(:invoice_item, 3)
 
     get "/api/v1/invoice_items"
@@ -13,5 +13,21 @@ RSpec.describe "Invoice Item API" do
 
     expect(invoice_items.count).to eq(3)
     expect(invoice_item).to have_key("id")
+    expect(invoice_item).to have_key("item_id")
+    expect(invoice_item).to have_key("invoice_id")
+    expect(invoice_item).to have_key("quantity")
+    expect(invoice_item).to have_key("unit_price")
+  end
+
+  it "can return one invoice item by id" do
+    id = create(:invoice_item).id
+
+    get "/api/v1/invoice_items/#{id}"
+
+    expect(response).to be_success
+
+    invoice_item = JSON.parse(response.body)
+
+    expect(invoice_item["id"]).to eq(id)
   end
 end
